1daea5d5d1f14d9bcbbd91eea796f653a9a6e349
artur <aptypkaa@gmail.com>
2021-09-23 10:24:25 +0300

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
-import React from 'react'
-
+import React, { useEffect, useState } from "react";
 
 function App() {
-  return "hi world";
+  const [item, setItem] = useState("");
+  const [list, setList] = useState([])
+
+  function formSubmit(e) {
+    e.preventDefault()
+    setList([...list, item])
+    setItem("")
+  }
+
+  function removeItemFromList(params){
+    const operation1 = list.filter((el,index) => el[index] !== el[params])
+    console.log(operation1)
+    setList(operation1)
+  }
+
+
+  return (
+    <>
+      <form onSubmit={formSubmit}>
+        <input
+          onChange={(e) => setItem(e.currentTarget.value)}
+          value={item}
+          type="text"
+        />
+        <button type="submit">save</button>
+      </form>
+      {list.length > 0 && list.map((el,index)=>{
+        return (<>
+        <div key={index}>{el} <button onClick={() => removeItemFromList(index)}>remove</button></div>
+        </>)
+      })}
+    </>
+  );
 }
 
 export default App;
