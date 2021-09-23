6a03ea67c19e642ed1f63c01c2ecbfedc5cd86c7
artur <aptypkaa@gmail.com>
2021-09-24 02:00:09 +0300

    update

--- README.md
+++ README.md
@@ -1,5 +1,14 @@
 CRA  
-npm run build  
-npm run test  
+
+simple task manager  
+
+demo here https://argdev.ru/proj/test/task-manager/  
+
+you can add tasks and remove them  
+you can highlight them onclick to make them more valuable  
+you can run the script on localhost by removing the line "homepage": "https://argdev.ru/proj/test/task-manager" from package.json
+the results will be saved to localstorage and recover when you reload the page  
+
 npm run start  
+npm run build  
 npm run eject  
--- src/App.js
+++ src/App.js
@@ -1,42 +1,93 @@
 import React, { useEffect, useState } from "react";
+import clsx from "clsx";
+import "./index.css";
 
 function App() {
   const [item, setItem] = useState("");
-  const [list, setList] = useState([])
+  const [list, setList] = useState(function () {
+    let todoList = [];
+    try {
+      todoList = JSON.parse(localStorage.getItem("todoList")) || [];
+    } catch (error) {
+      todoList = [];
+    }
+    return todoList;
+  });
+
+  useEffect(() => {
+    localStorage.setItem("todoList", JSON.stringify(list));
+  }, [list]);
+
+  function removeItemFromList(e, params) {
+    e.stopPropagation();
+    setList(list.filter((el, index) => index !== params));
+  }
 
   function formSubmit(e) {
-    e.preventDefault()
-    setList([...list, item])
-    setItem("")
+    e.preventDefault();
+    setList([...list, { item, active: false }]);
+    setItem("");
   }
 
-  function removeItemFromList(params){
-    const operation1 = list.filter((el,index) => index !== params)
-    console.log(operation1)
-    setList(operation1)
+  function toggleClass(params) {
+    setList([
+      ...list.map((el, index) => {
+        if (index === params) {
+          return { ...el, active: !el.active };
+        } else {
+          return { ...el };
+        }
+      }),
+    ]);
   }
-
-useEffect(() => {
-  console.log(list)
-}, [list])
-
-
 
   return (
     <>
-      <form onSubmit={formSubmit}>
-        <input
-          onChange={(e) => setItem(e.currentTarget.value)}
-          value={item}
-          type="text"
-        />
-        <button type="submit">save</button>
-      </form>
-      {list.map((el,index)=>{
-        return (<>
-        <div key={index}>{el} <button onClick={() => removeItemFromList(index)}>remove</button></div>
-        </>)
-      })}
+      <div className={"container"}>
+        <div className={"formWrapper"}>
+          <form onSubmit={formSubmit}>
+            <input
+              className={"inputAdd"}
+              onChange={(e) => setItem(e.currentTarget.value)}
+              value={item}
+              type="text"
+            />
+            <button className={"btnAdd"} type="submit">
+              add
+            </button>
+          </form>
+        </div>
+        <div className={"listWrapper"}>
+          {list.map((el, index) => {
+            return (
+              <>
+                <div
+                  onClick={() => toggleClass(index)}
+                  className={clsx({
+                    itemWrapper: true,
+                    listItemActive: el.active,
+                  })}
+                >
+                  <button
+                    className={"btnRemove"}
+                    onClick={(e) => removeItemFromList(e, index)}
+                  >
+                    remove
+                  </button>
+                  <div
+                    className={clsx({
+                      listItem: true,
+                    })}
+                    key={index}
+                  >
+                    {el.item}
+                  </div>
+                </div>
+              </>
+            );
+          })}
+        </div>
+      </div>
     </>
   );
 }
